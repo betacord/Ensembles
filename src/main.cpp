97e121c3fbb6b336f739867aa747558c77291964
Tomasz Krzywicki <t-krzywicki@wp.pl>
2018-07-16 14:08:51 +0200

    DecisionSystem class and tests for this class have been added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "../include/DecisionSystem.h"
+
+
+int main(int argc, char* argv[]) {
+
+    testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
+
     return 0;
 }
